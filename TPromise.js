8183126be620b25fd2333bd644276efd7cff9b2a
liyefei737 <macrofly@gmail.com>
2020-08-18 13:40:48 -0700

    .then .catch done

--- TPromise.js
+++ TPromise.js
@@ -1,30 +1,107 @@
 // If a promise object has a state of "Fufilled" or "Rejected", It's "Settled"
 const PromiseState = {
-    Pending: "Pending",
-    Fufilled: "Fufilled",
-    Rejected: "Rejected"
+    PENDING: "Pending",
+    FUFILLED: "Fufilled",
+    REJECTED: "Rejected"
 }
 
-class TPromise{
-    
+const isObjAPromise = obj => obj && typeof obj.then === "function";
+
+class TPromise {
+
     constructor(computation) {
-        this._state = PromiseState.Pending;
+        this._state = PromiseState.PENDING;
         this._value = null;
         this._reason = null;
+        this.thenQueue = []; // support multiple .then on the promise
+        this.catchQueue = []; // support multiple .catch on the promise
         if (typeof computation === "function") {
-            setTimeout(() =>{
+            setTimeout(() => {
                 try {
                     computation(
                         this._fufill.bind(this),
                         this._reject.bind(this)
                     );
                 } catch (error) {
-                    
+
                 }
             })
         }
     }
-    
+
+    then(fufillFn, failFn) {
+        const newPromise = new TPromise();
+        this.thenQueue.push([newPromise, fufillFn, failFn]);
+        return newPromise;
+
+
+    }
+
+    catch(catchFn) {
+        return this.then(undefined, catchFn);
+    }
+
+    finally() {
+
+    }
+
+    _fufill(value) {
+        this._state = PromiseState.FUFILLED;
+        this._value = value;
+        this._broadcastFufilledVal();
+    }
+
+    _reject(reason) {
+        this._state = PromiseState.REJECTED;
+        this._reason = reason;
+        this._broadcastRejectedReason();
+    }
+
+    _broadcastFufilledVal() {
+        this.thenQueue.forEach(([childPromise, fufillFn]) => {
+            if (typeof fufillFn === 'function') {
+                const result = fufillFn(this._value);
+                if (isObjAPromise(result)) {
+                    // if a promise obj is given to resolve
+                    // we wait for that promise to resolve
+                    result.then(
+                        value => childPromise._fufill(value),
+                        reason => childPromise._reject(reason)
+                    );
+                } else {
+                    childPromise._fufill(result);
+                }
+            } else {
+                // no function given to .then
+                childPromise._fufill(this._value);
+            }
+        })
+
+        // the first fufill or reject takes precendence
+        // the fufills or rejects that follow will get empty array 
+        this.thenQueue = [];
+    }
+
+    _broadcastRejectedReason() {
+        this.thenQueue.forEach(([childPromise, _, catchFn]) => {
+            if (typeof catchFn === 'function') {
+                const result = catchFn(this._reason);
+                if (isObjAPromise(result)) {
+                    result.then(
+                        value => childPromise._fufill(value),
+                        reason => childPromise._reject(reason)
+                    );
+                } else {
+                    // this allows .then after .catch
+                    childPromise._fufill(result);
+                }
+            } else {
+                // no function given to .then
+                childPromise._reject(this._reason);
+            }
+        })
+        this.thenQueue = [];
+    }
 
 }
 
--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-
+const TPromise = require('./TPromise.js');
 
 
 const p = new TPromise((resolve, reject) => {
-   
+
     setTimeout(() => {
         resolve(3);
     }, 1);
 });
+
+
+const then1 = p.then(v => console.log(v + 1));
+const then2 = p.then(v => console.log(v + 2));
+
+
+const then3 = p.then(v => { console.log(v + 1); return v + 1; });
+const then4 = then3.then(v => console.log(v + 1));
+
+
+
+const q = new TPromise((resolve, reject) => {
+
+    setTimeout(() => {
+        reject("something went wrong");
+    }, 1);
+});
+
+const catchPromise = q.catch((reason) => {
+    console.log(reason);
+    return 66;
+});
+
+
+catchPromise.then((val) => {
+    console.log(val);
+})
